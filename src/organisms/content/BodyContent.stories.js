bb0d2363391c2ee051412720fa76de730d9df823
Emmanuel Mathi-Amorim <manny@amorim.ca>
2023-05-17 13:45:04 -0400

    Plugin cleanup

--- src/organisms/content/BodyContent.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-
-import BodyContent from './BodyContent'
-
-storiesOf('organisms/content/BodyContent', module).addWithJSX('Default', () => {
-  return <BodyContent>Body content goes here...</BodyContent>
-})
